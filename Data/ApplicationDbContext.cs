6e9c5a7c9d557022a15e74743d8d4ac3f67af198
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-17 16:43:18 +0545

    created connection string to establish connection with sql server so use entity framework core by creating object of db context and using that object we will be able to create connection to the database and created DBSet method to create table in database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using fitness.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fitness.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //in constructor we will receive some options and pass to the base class i.e. DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  //configuring dbContext
+        {
+
+        }
+        public DbSet<User> Users { get; set; }  //create Users table in database using DbSet
+    }
+}
