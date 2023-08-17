fd00450dcc3246a9b66f339aba743e7eaf412c8f
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-17 14:54:24 +0545

    entity framework -- data anotation for user

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,16 @@
-﻿namespace fitness.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fitness.Models
 {
     public class User
     {
+        [Key]
         public int id { get; set; }
+        [Required]
         public string name { get; set; }
+        [Required]
         public string email { get; set; }
+        [Required]
         public string password { get; set; }
     }
 }
