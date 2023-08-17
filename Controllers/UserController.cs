8e435efcd1ff42c07838d14abaee3c19fa2248ce
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-17 17:44:15 +0545

    retrive user from database

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using fitness.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace fitness.Controllers
 {
     public class UserController : Controller
     {
+
+        //request applicataion to send the object of ApplicationDbContext to work with database
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db) //can access whatever is register inside the container i.e. the two line of code in program.cs as parameter in constructor
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var objUserList = _db.Users.ToList();
             return View();
         }
     }
